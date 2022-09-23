7a250385957a90625772f51098e52ade7d0ba532
Katie Walters <katie_walters@live.co.uk>
2022-09-23 18:50:40 +0100

    updated data file for grades 1 - 5 clarinet

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-
+var cors = require('cors')
 const express = require('express');
 const mongoose = require('mongoose');
 const mongoString = process.env.DATABASE_URL;
@@ -17,7 +17,7 @@ database.once('connected', () => {
 const app = express();
 
 app.use(express.json());
-
+app.use(cors())
 
 app.listen(3000, () => {
     console.log(`Server Started at ${3000}`)
